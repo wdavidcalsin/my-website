a9b3bef9e23d3cd0651cb0ba01c0c9ea3e705307
WDavid <wdavidcalsin968@gmail.com>
2023-02-14 01:20:30 -0500

    feat: 🎸 add notion connection and display in project section

--- src/components/projects-section/projects-section.tsx
+++ src/components/projects-section/projects-section.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Box, Chip, Stack, Typography } from '@mui/material';
 import FaceIcon from '@mui/icons-material/Face';
 import {
@@ -9,8 +10,21 @@ import {
   timelineItemClasses,
   TimelineSeparator,
 } from '@mui/lab';
+import { INotionService } from '@/types';
+import { notionProjectService } from '@/services';
 
 const ProjectsSection = () => {
+  const [listProject, setListProject] = React.useState<INotionService[]>([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const resultNotion = await notionProjectService();
+      setListProject(resultNotion);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Stack spacing={2}>
       <Typography
@@ -31,138 +45,68 @@ const ProjectsSection = () => {
           },
         }}
       >
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Stack
-              spacing={2}
-              sx={{
-                border: '1px solid',
-                borderColor: 'custom.border',
-                borderRadius: '1rem',
-                padding: '1rem',
-              }}
-            >
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    fontWeight: 'bold',
-                    fontSize: '1.2rem',
-                    textTransform: 'capitalize',
-                    color: 'secondary.contrastText',
-                    paddingBottom: '.8rem',
-                  }}
-                >
-                  dictionary
-                </Typography>
+        {listProject.map((itemProject) => (
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Stack
+                spacing={2}
+                sx={{
+                  border: '1px solid',
+                  borderColor: 'custom.border',
+                  borderRadius: '1rem',
+                  padding: '1rem',
+                }}
+              >
+                <Box>
+                  <Typography
+                    variant="h2"
+                    sx={{
+                      fontWeight: 'bold',
+                      fontSize: '1.2rem',
+                      textTransform: 'capitalize',
+                      color: 'secondary.contrastText',
+                      paddingBottom: '.8rem',
+                    }}
+                  >
+                    {itemProject.title}
+                  </Typography>
+                  <Stack direction="row" spacing={1}>
+                    <Chip
+                      label="Github"
+                      sx={{ color: '#2D3748', bgcolor: '#CBD5E0' }}
+                    />
+                    <Chip
+                      label="Twitter"
+                      variant="outlined"
+                      sx={{ color: '#2C5282', bgcolor: '#90CDF4' }}
+                    />
+                    <Chip
+                      label="Linkedin"
+                      variant="outlined"
+                      sx={{ color: '#2C5282', bgcolor: '#90CDF4' }}
+                    />
+                  </Stack>
+                </Box>
+                <Box>
+                  <Typography
+                    variant="body1"
+                    sx={{ color: 'secondary.contrastText' }}
+                  >
+                    {itemProject.description}
+                  </Typography>
+                </Box>
                 <Stack direction="row" spacing={1}>
-                  <Chip
-                    label="Github"
-                    sx={{ color: '#2D3748', bgcolor: '#CBD5E0' }}
-                  />
-                  <Chip
-                    label="Twitter"
-                    variant="outlined"
-                    sx={{ color: '#2C5282', bgcolor: '#90CDF4' }}
-                  />
-                  <Chip
-                    label="Linkedin"
-                    variant="outlined"
-                    sx={{ color: '#2C5282', bgcolor: '#90CDF4' }}
-                  />
+                  <Chip icon={<FaceIcon />} label="Site" />
+                  <Chip icon={<FaceIcon />} label="Code" variant="outlined" />
                 </Stack>
-              </Box>
-              <Box>
-                <Typography
-                  variant="body1"
-                  sx={{ color: 'secondary.contrastText' }}
-                >
-                  Code Magic is an open-source application that simplifies and
-                  saves users time and effort during the project creation
-                  process by providing a user-friendly tool for creating
-                  projects with advanced CSS features.
-                </Typography>
-              </Box>
-              <Stack direction="row" spacing={1}>
-                <Chip icon={<FaceIcon />} label="Site" />
-                <Chip icon={<FaceIcon />} label="Code" variant="outlined" />
               </Stack>
-            </Stack>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Stack
-              spacing={2}
-              sx={{
-                border: '1px solid',
-                borderColor: 'custom.border',
-                borderRadius: '1rem',
-                padding: '1rem',
-              }}
-            >
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    fontWeight: 'bold',
-                    fontSize: '1.2rem',
-                    textTransform: 'capitalize',
-                    color: 'secondary.contrastText',
-                    paddingBottom: '.8rem',
-                  }}
-                >
-                  dictionary
-                </Typography>
-                <Stack direction="row" spacing={1}>
-                  <Chip
-                    label="Github"
-                    sx={{ color: '#2D3748', bgcolor: '#CBD5E0' }}
-                  />
-                  <Chip
-                    label="Twitter"
-                    variant="outlined"
-                    sx={{ color: '#2C5282', bgcolor: '#90CDF4' }}
-                  />
-                  <Chip
-                    label="Linkedin"
-                    variant="outlined"
-                    sx={{ color: '#2C5282', bgcolor: '#90CDF4' }}
-                  />
-                </Stack>
-              </Box>
-              <Box>
-                <Typography
-                  variant="body1"
-                  sx={{ color: 'secondary.contrastText' }}
-                >
-                  Code Magic is an open-source application that simplifies and
-                  saves users time and effort during the project creation
-                  process by providing a user-friendly tool for creating
-                  projects with advanced CSS features.
-                </Typography>
-              </Box>
-              <Stack direction="row" spacing={1}>
-                <Chip icon={<FaceIcon />} label="Site" />
-                <Chip icon={<FaceIcon />} label="Code" variant="outlined" />
-              </Stack>
-            </Stack>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Willian David</TimelineContent>
-        </TimelineItem>
+            </TimelineContent>
+          </TimelineItem>
+        ))}
       </Timeline>
     </Stack>
   );
--- src/request/api-notion/api-notion.ts
+++ src/request/api-notion/api-notion.ts
@@ -1,11 +1,15 @@
 import { INotionRequest } from '@/types';
 
-const NOTION_URL_API = 'https://my-website-server-topaz.vercel.app/api';
+const NOTION_URL_API = 'https://my-website-server-topaz.vercel.app/api/';
 
 export const apiNotion = async (): Promise<INotionRequest[]> => {
   const resApi = await fetch(NOTION_URL_API);
 
+  if (!resApi) {
+    console.log('Notion api not found');
+  }
+
   const listProjects = await resApi.json();
 
-  return JSON.parse(listProjects);
+  return listProjects;
 };
