4bb84922ef1bfca7fd0881411bd6caffb9b0edb6
WDavid <david2c142@gmail.com>
2024-04-08 16:47:35 -0500

    refactor: update the information on the project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,12 +13,12 @@ const Page: React.FC<PageProps> = async () => {
             <div>
                 <header className="max-w-2xl">
                     <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-                        Cosas que he hecho para mostrar mi trabajo
+                        Things I have done to showcase my work.
                     </h1>
                     <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                        Hey trabajado en diferentes proyectos personales, que me
-                        permitieron descubrir tecnologias y algunas maneras de
-                        desarrollar software, aqui algunos de ellos:
+                        I have worked on different personal projects, which
+                        allowed me to discover technologies and various ways of
+                        developing software. Here are some of them:
                     </p>
                 </header>
                 <ProjectsSection projects={projects} />
