3026903fac150e650b25b01951a547287c346722
WDavid <david2c142@gmail.com>
2024-06-05 22:16:09 -0500

    feat: add animation in navbar

--- app/components/navigation/list-navigation.tsx
+++ app/components/navigation/list-navigation.tsx
@@ -1,7 +1,9 @@
+import * as React from "react";
 import { menuItems } from "@/app/constants/menu-navigation";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { MagicTabSelect } from "react-magic-motion";
 
 interface ListNavigationProps {
     variant?: "full" | "mobile";
@@ -13,6 +15,17 @@ const ListNavigation: React.FC<ListNavigationProps> = ({
     onclick,
 }) => {
     const pathname = usePathname();
+    const [hoveredIndex, setHoveredIndex] = React.useState(pathname);
+    const [selectIndex, setSelectIndex] = React.useState(pathname);
+
+    console.log(pathname);
+
+    React.useEffect(() => {
+        if (pathname === "/") {
+            setHoveredIndex("/");
+            setSelectIndex("/");
+        }
+    }, [pathname]);
 
     return (
         <>
@@ -26,11 +39,45 @@ const ListNavigation: React.FC<ListNavigationProps> = ({
                             pathname === item.href ? "text-[#28BEAE] " : "",
                             variant === "full" ? "" : "text-center"
                         )}
-                        onClick={onclick}
+                        onClick={() => {
+                            if (onclick) {
+                                onclick();
+                            }
+
+                            setSelectIndex(item.href);
+                            // setHoveredIndex(item.href);
+                        }}
+                        onMouseEnter={() => setHoveredIndex(item.href)}
+                        onMouseLeave={() => {
+                            if (hoveredIndex !== selectIndex)
+                                setHoveredIndex(selectIndex);
+                        }}
                     >
                         {item.title}
-                        {pathname === item.href && (
-                            <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>
+                        {hoveredIndex === item.href && (
+                            <MagicTabSelect
+                                id="nav-select"
+                                // key={pathname}
+                                transition={{
+                                    type: "spring",
+                                    bounce: 0.35,
+                                }}
+                            >
+                                <span className="absolute right-0 left-0 inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0 mix-blend-difference" />
+                                {/* <span
+                                    style={{
+                                        borderRadius: "999px",
+                                        position: "absolute",
+                                        top: 0,
+                                        left: 0,
+                                        right: 0,
+                                        bottom: 0,
+                                        zIndex: 10,
+                                        backgroundColor: "white",
+                                        mixBlendMode: "difference",
+                                    }}
+                                /> */}
+                            </MagicTabSelect>
                         )}
                     </Link>
                 );
--- app/components/navigation/menu-navigation.tsx
+++ app/components/navigation/menu-navigation.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from "react";
-import ListNavigation from "./list-navigation";
 import ChevronDownIcon from "../icons/chevron-down-icon";
+import ListNavigation from "./list-navigation";
 
 interface MenuNavigationProps {
     onClick?: () => void;
@@ -11,7 +11,7 @@ interface MenuNavigationProps {
 const MenuNavigation: React.FC<MenuNavigationProps> = ({ onClick }) => {
     return (
         <div>
-            <div className="hidden sm:flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+            <div className="hidden sm:flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 mt-14">
                 <ListNavigation />
             </div>
 
